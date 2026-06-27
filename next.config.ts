56f90683e091657f8a3a7297f9c1ca91b914d789
SDRCasagrande <casagrandesdr@gmail.com>
2026-06-27 16:10:16 -0300

    fix: Adiciona allowedOrigins para Server Actions no painel admin (Evita erro 405 CSRF)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  serverExternalPackages: ['bcryptjs'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'borapassageiroxinguara.com.br',
+        '*.borapassageiroxinguara.com.br',
+      ]
+    }
+  }
 };
 
 export default nextConfig;
